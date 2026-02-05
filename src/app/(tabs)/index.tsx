7f4819d7de5fd91a65f68019dd1489944357df93
albina0603 <asanovaalbina3@github.com>
2026-02-05 13:51:30 +0600

    first

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
+import Greeting from "@/components/Greeting";
+import ProfileCard from "@/components/ProfileCard";
 import { View } from "react-native";
 
 export default function HomeScreen() {
-  return <View style={{ flex: 1, backgroundColor: "#fff" }}></View>;
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <Greeting />
+      <ProfileCard name="name"/>
+    </View>
+  );
 }
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,4 @@
+import { Text } from "@react-navigation/elements";
+export default function Greeting() {
+  return <Text>Hgggi</Text>;
+}
