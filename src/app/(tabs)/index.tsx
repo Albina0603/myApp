259fadc77ee82ca4d7d6740a0a8eb3bd49768e07
albina0603 <asanovaalbina3@github.com>
2026-02-16 15:33:10 +0600

    kkkkkk

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
 import CounterButton from "@/components/CounterButton";
-import { ProfileCard } from "@/components/ProfileCard";
 import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+    /*<View style={{ flex: 1, backgroundColor: "#fff" }}>
       <CounterButton title="Sign in " step={1} initialValue={0}/>
       <ProfileCard name="Albinaaa" role="studentka molodenkaya" course="4"></ProfileCard>
+    </View>
+  );
+}*/
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      {/* <ProfileCard name="asd" course="DEV" role="Student" btnName="Press me" btnStep={1056} */}
+
+      <CounterButton title="Click Me1" step={1} initialValue={0} />
+      <CounterButton title="Click Me2" step={2} initialValue={0} />
+      <CounterButton title="Click Me3" step={3} initialValue={0} />
+      <CounterButton title="Click Me4" step={4} initialValue={0} />
+
+      {/* <Greeting name="asd" role="teacher"/> */}
     </View>
   );
 }
