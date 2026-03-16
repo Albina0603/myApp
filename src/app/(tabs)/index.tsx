76f4edfc923a208e1caddc81031fe8f73f99e739
albina0603 <asanovaalbina3@github.com>
2026-03-16 15:24:09 +0600

    classwork

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,22 @@
-import CounterButton from "@/components/CounterButton";
+import TodoScreen from "@/screens/Todoscreen";
 import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
     /*<View style={{ flex: 1, backgroundColor: "#fff" }}>
       <CounterButton title="Sign in " step={1} initialValue={0}/>
-      <ProfileCard name="Albinaaa" role="studentka molodenkaya" course="4"></ProfileCard>
+      <ProfileCard name="Albinaaa" role="studentka " course="4"></ProfileCard>
     </View>
   );
 }*/
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <TodoScreen></TodoScreen>
       {/* <ProfileCard name="asd" course="DEV" role="Student" btnName="Press me" btnStep={1056} */}
 
-      <CounterButton title="Click Me1" step={1} initialValue={0} />
+      {/* <CounterButton title="Click Me1" step={1} initialValue={0} />
       <CounterButton title="Click Me2" step={2} initialValue={0} />
       <CounterButton title="Click Me3" step={3} initialValue={0} />
-      <CounterButton title="Click Me4" step={4} initialValue={0} />
+      <CounterButton title="Click Me4" step={4} initialValue={0} /> */}
 
       {/* <Greeting name="asd" role="teacher"/> */}
     </View>
